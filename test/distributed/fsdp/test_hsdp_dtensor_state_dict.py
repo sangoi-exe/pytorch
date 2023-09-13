sangoi-exe/pytorch

commit 5d90148171c4dff4e4c5598add6b8c64edd47b75

[HSDP] Add device_mesh to FSDP and add dtensor state_dict support for HSDP (#107533)
This PR:
1) Add device_mesh kwarg to FSDP. Remove init_device_mesh() from _runtime_utils.py, as device_mesh would be passed in by user as an kwarg.
2) change use_dtensor flag for state_dict_config and optim_state_dict_config to be private. If device_mesh is used with sharded model/optim state dict, _use_dtensor flag would be set to True and model/optim state dict would return dtensor state_dict. Otherwise, _use_dtensor flag would be set to False and model/optim state dict would return sharded_tensor state_dict.
3) Update _optim_utils.py, _shard_utils.py, and _state_dict_utils.py to add support for HSDP to return 2D DTensor state_dict.

Pull Request resolved: https://github.com/pytorch/pytorch/pull/107533
Approved by: https://github.com/fegin, https://github.com/awgu, https://github.com/wanchaol

test/distributed/fsdp/test_hsdp_dtensor_state_dict.py
@@ -7,12 +7,13 @@ import torch
 import torch.nn as nn
 from torch.distributed._shard.sharded_tensor import ShardedTensor
 
-from torch.distributed._tensor import DTensor, Shard
+from torch.distributed._tensor import DTensor, Replicate, Shard
 from torch.distributed._tensor.device_mesh import init_device_mesh
 from torch.distributed.fsdp import FullyShardedDataParallel as FSDP
 from torch.distributed.fsdp.api import (
     ShardedOptimStateDictConfig,
     ShardedStateDictConfig,
+    ShardingStrategy,
     StateDictType,
 )
 from torch.testing._internal.common_utils import (
@@ -43,12 +44,33 @@ class TestDummyModel(torch.nn.Module):
         return self.net4(self.net3(self.net2(self.net1(x))))
 
     def get_input(self):
-        return torch.rand(8, 8, device="cuda")
+        return torch.rand(4, 8, device="cuda")
 
 
-class TestFSDPWithDeviceMeshAndDTensor(DTensorTestBase):
-    def _create_model(self, device_mesh=None):
-        model = FSDP(TestDummyModel().cuda(), device_mesh=device_mesh)
+# TODO: Consolidate DeviceMesh based FSDP and HSDP test cases.
+class TestHSDPWithDeviceMeshAndDTensor(DTensorTestBase):
+    @property
+    def world_size(self):
+        return 4
+
+    def _create_model(self, use_device_mesh=True):
+        device_mesh = init_device_mesh(self.device_type, (2, 2))
+
+        if use_device_mesh:
+            model = FSDP(
+                TestDummyModel().cuda(),
+                device_mesh=device_mesh,
+                sharding_strategy=ShardingStrategy.HYBRID_SHARD,
+            )
+        else:
+            intra_node_pg = device_mesh.get_dim_groups(mesh_dim=1)
+            inter_node_pg = device_mesh.get_dim_groups(mesh_dim=0)
+            model = FSDP(
+                TestDummyModel().cuda(),
+                process_group=(intra_node_pg, inter_node_pg),
+                sharding_strategy=ShardingStrategy.HYBRID_SHARD,
+            )
+
         optim = torch.optim.Adam(model.parameters(), lr=0.1)
         model(model.get_input()).sum().backward()
         optim.step()
@@ -56,10 +78,11 @@ class TestFSDPWithDeviceMeshAndDTensor(DTensorTestBase):
         return model, optim
 
     @with_comms
-    @skip_if_lt_x_gpu(2)
-    def test_fsdp_init_with_device_mesh(self):
-        device_mesh = init_device_mesh(self.device_type, (self.world_size,))
-        model, optim = self._create_model(device_mesh)
+    @skip_if_lt_x_gpu(4)
+    def test_hsdp_init_with_device_mesh(self):
+        mesh_tensor = torch.arange(self.world_size).view(2, -1)
+        device_mesh = DeviceMesh(self.device_type, mesh_tensor)
+        model, optim = self._create_model(use_device_mesh=True)
 
         FSDP.set_state_dict_type(
             model,
@@ -68,28 +91,26 @@ class TestFSDPWithDeviceMeshAndDTensor(DTensorTestBase):
             optim_state_dict_config=ShardedOptimStateDictConfig(),
         )
         state_dict_type = model.get_state_dict_type(model)
+
         # If device_mesh is used when initializing FSDP, the field _use_dtensor will
         # automatically be set to True.
         self.assertEqual(state_dict_type.state_dict_config._use_dtensor, True)
         self.assertEqual(state_dict_type.optim_state_dict_config._use_dtensor, True)
 
         for v in model.state_dict().values():
-            self.assertEqual(len(v.placements), 1)
-            self.assertEqual(v.placements[0], (Shard(dim=0)))
+            self.assertEqual(v.placements, (Replicate(), Shard(0)))
             self.assertEqual(v.device_mesh, device_mesh)
 
         for v in optim.state_dict()["state"].values():
             if isinstance(v, DTensor):
-                self.assertEqual(len(v.placements), 1)
-                self.assertEqual(v.placements[0], (Shard(dim=0)))
+                self.assertEqual(v.placements, (Replicate(), Shard(0)))
                 self.assertEqual(v.device_mesh, device_mesh)
 
     @with_comms
-    @skip_if_lt_x_gpu(2)
+    @skip_if_lt_x_gpu(4)
     @parametrize("offload_to_cpu", [True, False])
     def test_dtensor_sharded_tensor_state_dict_identical(self, offload_to_cpu):
-        device_mesh = init_device_mesh(self.device_type, (self.world_size,))
-        model, optim = self._create_model(device_mesh)
+        model, optim = self._create_model(use_device_mesh=True)
 
         FSDP.set_state_dict_type(
             model,
@@ -102,7 +123,7 @@ class TestFSDPWithDeviceMeshAndDTensor(DTensorTestBase):
         dtensor_sd = model.state_dict()
         dtensor_osd = FSDP.optim_state_dict(model, optim)
 
-        ref_model, ref_optim = self._create_model()
+        ref_model, ref_optim = self._create_model(use_device_mesh=False)
         FSDP.set_state_dict_type(
             ref_model,
             StateDictType.SHARDED_STATE_DICT,
@@ -146,11 +167,10 @@ class TestFSDPWithDeviceMeshAndDTensor(DTensorTestBase):
                     self.assertEqual(v1.to_local().device, v2.local_tensor().device)
 
     @with_comms
-    @skip_if_lt_x_gpu(2)
+    @skip_if_lt_x_gpu(4)
     @parametrize("offload_to_cpu", [True, False])
     def test_dtensor_sharded_optim_load_state_dict(self, offload_to_cpu):
-        device_mesh = init_device_mesh(self.device_type, (self.world_size,))
-        model, optim = self._create_model(device_mesh)
+        model, optim = self._create_model(use_device_mesh=True)
 
         FSDP.set_state_dict_type(
             model,
@@ -196,11 +216,10 @@ class TestFSDPWithDeviceMeshAndDTensor(DTensorTestBase):
                 self.assertEqual(v1, v2)
 
     @with_comms
-    @skip_if_lt_x_gpu(2)
+    @skip_if_lt_x_gpu(4)
     @parametrize("offload_to_cpu", [True, False])
     def test_dtensor_sharded_model_load_state_dict(self, offload_to_cpu):
-        device_mesh = init_device_mesh(self.device_type, (self.world_size,))
-        model, optim = self._create_model(device_mesh)
+        model, optim = self._create_model(use_device_mesh=True)
 
         FSDP.set_state_dict_type(
             model,
@@ -232,6 +251,6 @@ class TestFSDPWithDeviceMeshAndDTensor(DTensorTestBase):
             self.assertEqual(v1, v2)
 
 
-instantiate_parametrized_tests(TestFSDPWithDeviceMeshAndDTensor)
+instantiate_parametrized_tests(TestHSDPWithDeviceMeshAndDTensor)
 if __name__ == "__main__":
     run_tests()

test/distributed/fsdp/test_fsdp_hybrid_shard.py
@@ -106,7 +106,8 @@ class TestFSDPHybridShard(FSDPTest):
     def test_raises_manual_wrap_hybrid_shard_when_none_policy(self):
         model = MyModel().cuda()
         err_ctx = self.assertRaisesRegex(
-            ValueError, "requires explicit specification of process group"
+            ValueError,
+            "requires explicit specification of process group or device_mesh.",
         )
 
         with err_ctx:

torch/distributed/fsdp/_fsdp_extensions.py
@@ -116,7 +116,7 @@ def _ext_chunk_dtensor(
     # TODO: Address composability issue and remove the assertion.
     assert (
         _extensions is None
-    ), "Currently does not support composability when use_dtensor = True"
+    ), "Currently does not support composability when _use_dtensor = True"
     return _create_chunk_dtensor(
         tensor,
         rank,

torch/distributed/fsdp/_init_utils.py
@@ -24,6 +24,7 @@ import torch.distributed.fsdp._exec_order_utils as exec_order_utils
 import torch.distributed.fsdp._traversal_utils as traversal_utils
 import torch.distributed.fsdp.fully_sharded_data_parallel as fsdp_file
 import torch.nn as nn
+from torch.distributed._tensor import DeviceMesh
 from torch.distributed.algorithms._comm_hooks import default_hooks
 from torch.distributed.distributed_c10d import _get_default_group
 from torch.distributed.fsdp._common_utils import (
@@ -98,21 +99,36 @@ def _init_process_group_state(
     process_group: ProcessGroupType,
     sharding_strategy: ShardingStrategy,
     policy: Optional[_Policy],
+    device_mesh: Optional[DeviceMesh] = None,
 ) -> _FSDPState:
+    if process_group is not None and device_mesh is not None:
+        raise ValueError(
+            "Cannot pass both process_group and device_mesh at the "
+            "same time. Please just pass only one of them."
+        )
     is_hybrid_strategy = sharding_strategy in HYBRID_SHARDING_STRATEGIES
     if is_hybrid_strategy:
-        if process_group is None and policy is None:
+        if process_group is None and policy is None and device_mesh is None:
             # Raise an error here, since this is manual wrapping with no process group
             # passed in, there is no way to ensure all wrapped FSDP instances use the same
             # process groups.
             raise ValueError(
-                f"Manual wrapping with {sharding_strategy} requires explicit specification of process group."
+                f"Manual wrapping with {sharding_strategy}",
+                "requires explicit specification of process group or device_mesh.",
             )
-        state = _init_process_group_state_for_hybrid_shard(state, process_group)
+        else:
+            state = _init_process_group_state_for_hybrid_shard(
+                state, process_group, device_mesh
+            )
     else:
-        state.process_group = (
-            process_group if process_group is not None else _get_default_group()
-        )
+        if device_mesh:
+            state._device_mesh = device_mesh
+            state.process_group = device_mesh.get_dim_groups(mesh_dim=0)
+        else:
+            state.process_group = (
+                process_group if process_group is not None else _get_default_group()
+            )
+
     state.rank = state.process_group.rank()
     state.world_size = state.process_group.size()
     data_parallel_world_size = state.world_size
@@ -131,9 +147,23 @@ def _init_process_group_state(
 
 @no_type_check
 def _init_process_group_state_for_hybrid_shard(
-    state: _FSDPState, process_group
+    state: _FSDPState,
+    process_group: ProcessGroupType,
+    device_mesh: DeviceMesh,
 ) -> _FSDPState:
-    if process_group is None:
+    if device_mesh:
+        if _is_valid_hybrid_shard_device_mesh(device_mesh):
+            state._device_mesh = device_mesh
+            # We currently only allow _inter_node_pg to be the outermost dimension, and the
+            # process_group(intra_node) to be the innermost dimension.
+            state._inter_node_pg = device_mesh.get_dim_groups(mesh_dim=0)
+            state.process_group = device_mesh.get_dim_groups(mesh_dim=1)
+        else:
+            raise ValueError(
+                "Expected device_mesh to have ndim=2 "
+                f"but got {len(device_mesh.get_dim_groups())}"
+            )
+    elif process_group is None:
         default_group = _get_default_group()
         intra_node_group, inter_node_group = _init_intra_and_inter_node_groups(
             default_group, state._device_handle.device_count()
@@ -167,6 +197,11 @@ def _is_valid_hybrid_shard_pg_type(process_group: Any) -> bool:
         and len(process_group) == 2
         and all(isinstance(pg, dist.ProcessGroup) for pg in process_group)
     )
+
+
+@no_type_check
+def _is_valid_hybrid_shard_device_mesh(device_mesh: DeviceMesh) -> bool:
+    return isinstance(device_mesh, DeviceMesh) and device_mesh.ndim == 2
 
 
 @no_type_check

torch/distributed/fsdp/_optim_utils.py
@@ -285,7 +285,13 @@ def _unflatten_communicated_optim_state(
                 views = flat_param_views[state_name]
             optim_state: Union[torch.Tensor, ShardedTensor, DTensor] = next(views)
             if shard_state:
-                if not fsdp_state._optim_state_dict_config.use_dtensor:
+                osd_config = fsdp_state._optim_state_dict_config
+                if getattr(osd_config, "_use_dtensor", False):
+                    assert fsdp_state._device_mesh is not None
+                    optim_state = _ext_chunk_dtensor(
+                        optim_state, fsdp_state.rank, fsdp_state._device_mesh
+                    )
+                else:
                     assert fsdp_state.process_group is not None
                     optim_state = _ext_chunk_tensor(
                         optim_state,
@@ -294,12 +300,6 @@ def _unflatten_communicated_optim_state(
                         fsdp_state._device_handle.device_count(),
                         fsdp_state.process_group,
                     )
-                else:
-                    assert fsdp_state._device_mesh is not None
-                    optim_state = _ext_chunk_dtensor(
-                        optim_state, fsdp_state.rank, fsdp_state._device_mesh
-                    )
-
             unflat_state_param[state_name] = optim_state
 
         # Add zero-dimension tensor state: take the target rank's value
@@ -1641,7 +1641,13 @@ def _gather_orig_param_state(
             flat_param._shapes[param_idx]
         )
         if shard_state:
-            if not fsdp_state._optim_state_dict_config.use_dtensor:
+            osd_config = fsdp_state._optim_state_dict_config
+            if getattr(osd_config, "_use_dtensor", False):
+                assert fsdp_state._device_mesh is not None
+                value = _ext_chunk_dtensor(
+                    value, fsdp_state.rank, fsdp_state._device_mesh
+                )
+            else:
                 assert fsdp_state.process_group is not None
                 value = _ext_chunk_tensor(
                     value,
@@ -1649,11 +1655,6 @@ def _gather_orig_param_state(
                     fsdp_state.world_size,
                     fsdp_state._device_handle.device_count(),
                     fsdp_state.process_group,
-                )
-            else:
-                assert fsdp_state._device_mesh is not None
-                value = _ext_chunk_dtensor(
-                    value, fsdp_state.rank, fsdp_state._device_mesh
                 )
         value = value.cpu()
         gathered_state[state_name] = value

torch/distributed/fsdp/_runtime_utils.py
@@ -11,8 +11,6 @@ import torch.nn as nn
 import torch.nn.functional as F
 from torch.autograd import Variable
 from torch.autograd.graph import register_multi_grad_hook
-from torch.distributed import get_backend, get_world_size
-from torch.distributed._tensor import DeviceMesh
 from torch.distributed.algorithms._comm_hooks import LOW_PRECISION_HOOKS
 from torch.distributed.fsdp._common_utils import (
     _assert_in_training_states,
@@ -198,24 +196,6 @@ def _check_flat_params_on_expected_device(state: _FSDPState, module: nn.Module):
             )
 
 
-def _init_device_mesh(
-    root_state: _FSDPState,
-) -> Optional[DeviceMesh]:
-    # We are testing 1D DeviceMesh where dist.get_world_size(pg) == dist.get_world_size() for now.
-    # TODO: Address cases when dist.get_world_size(pg) != dist.get_world_size(). This would capture
-    #       what 1D DeviceMesh currently would not work for:
-    #       1) HSDP Hybrid Sharding, 2) 2D FSDP + TP, 3) dist.new_group() cannot be expressed in 1D DeviceMesh.
-    if root_state.process_group != dist.distributed_c10d._get_default_group():
-        return None
-    if get_backend() == "fake" or not root_state.compute_device:
-        return None
-
-    device_type = root_state.compute_device.type
-    mesh_tensor = torch.arange(get_world_size(root_state.process_group))
-    device_mesh = DeviceMesh(device_type, mesh_tensor, _validate_mesh=False)
-    return device_mesh
-
-
 @no_type_check
 def _share_state_and_init_handle_attrs(
     root_state: _FSDPState,
@@ -234,7 +214,6 @@ def _share_state_and_init_handle_attrs(
     for attr_name in HOMOGENEOUS_ATTR_NAMES:
         attr_name_to_values[attr_name] = set()
     root_state._all_handles = root_state._exec_order_data.all_handles  # share reference
-    root_state._device_mesh = _init_device_mesh(root_state)
     # Update _has_optim_in_backward for each handle.
     for handle in root_state._all_handles:
         flat_param = handle.flat_param
@@ -270,10 +249,13 @@ def _share_state_and_init_handle_attrs(
         fsdp_state._default_stream = root_state._default_stream
         fsdp_state._exec_order_data = root_state._exec_order_data
         fsdp_state._free_event_queue = root_state._free_event_queue
-        fsdp_state._device_mesh = root_state._device_mesh
         handle = fsdp_state._handle
         if handle:
             handle.init_flat_param_attributes()
+        # TODO: remove this if check after we integrate device_mesh in Composable APIs.
+        # Currently, it is needed since root_state of composable APIs doesn't have DeviceMesh passed in yet.
+        if hasattr(root_state, "_device_mesh"):
+            fsdp_state._device_mesh = root_state._device_mesh
     for attr_name, attr_values in attr_name_to_values.items():
         if len(attr_values) != 1:
             raise ValueError(

torch/distributed/fsdp/_shard_utils.py
@@ -1,3 +1,4 @@
+import copy
 import itertools
 import math
 from typing import Any, Dict, Optional
@@ -50,6 +51,8 @@ def _all_gather_sharded_tensor(
     return tensor
 
 
+# TODO: Make this API work for both FSDP, and 2D. Move it outside of FSDP.
+# External users are interesting in using this API.
 def _gather_state_dict(
     state_dict: Dict[str, Any],
     pg: Optional[dist.ProcessGroup] = None,
@@ -73,8 +76,13 @@ def _gather_state_dict(
         elif isinstance(tensor, DTensor):
             if tensor.device != tensor.device_mesh.device_type:
                 tensor = tensor.to(tensor.device_mesh.device_type)
+            # FSDP all_gather: [Shard(0)] -> [Replicate()]
+            # HSDP all_gather: [Replicate(), Shard(0)] -> [Replicate(), Replicate()]
+            placements = list(copy.deepcopy(tensor.placements))
+            placements[-1] = Replicate()
             tensor = tensor.redistribute(
-                device_mesh=tensor.device_mesh, placements=[Replicate()]
+                device_mesh=tensor.device_mesh,
+                placements=placements,
             )
             tensor = tensor.to_local()
         new_state_dict[key] = tensor
@@ -150,10 +158,11 @@ def _create_chunk_dtensor(
     Shard a tensor to chunks along the first dimension. The local rank will gets its
     corresponding chunk as the local tensor to create a DTensor.
     """
+    inner_dim = device_mesh.ndim - 1
     shard_placement = DShard(0)
     tensor_list, _ = shard_placement._split_tensor(
         tensor,
-        device_mesh.size(dim=0),
+        device_mesh.size(dim=inner_dim),
         with_padding=False,
         contiguous=True,
     )
@@ -161,4 +170,9 @@ def _create_chunk_dtensor(
     # Each chunk is a view of the input tensor. If the original tensor change, the view will also be changed.
     # We need to explicitly call .detach() to return a new tensor detached from the current graph.
     local_tensor = tensor_list[rank].clone().detach()
-    return DTensor.from_local(local_tensor, device_mesh, [shard_placement])
+
+    # FSDP placements: [Shard(0)]
+    # HSDP placements: [Replicate(), Shard(0)]
+    placements = [Replicate() for _ in range(device_mesh.ndim)]
+    placements[-1] = shard_placement  # type: ignore[call-overload]
+    return DTensor.from_local(local_tensor, device_mesh, placements)

torch/distributed/fsdp/_state_dict_utils.py
@@ -1,4 +1,5 @@
 import contextlib
+import copy
 import logging
 import math
 import warnings
@@ -538,7 +539,7 @@ def _sharded_post_state_dict_hook(
 
     def param_hook(state_dict: Dict[str, Any], prefix: str, fqn: str):
         param = state_dict[fqn]
-        if not fsdp_state._state_dict_config.use_dtensor:
+        if not fsdp_state._state_dict_config._use_dtensor:
             sharded_tensor = _ext_chunk_tensor(
                 tensor=param,
                 rank=fsdp_state.rank,
@@ -601,7 +602,7 @@ def _sharded_pre_load_state_dict_hook(
             fqn_from_global_root = f"{prefix}{fqn}"
         param = state_dict.pop(fqn_from_global_root)
 
-        if not fsdp_state._state_dict_config.use_dtensor:
+        if not fsdp_state._state_dict_config._use_dtensor:
             # All-gather the param (ShardedTensor)
             param, shards = _ext_pre_load_state_dict_transform(param)
 
@@ -652,9 +653,11 @@ def _sharded_pre_load_state_dict_hook(
         else:
             if param.device != fsdp_state._device_mesh.device_type:
                 param = param.to(fsdp_state._device_mesh.device_type)
-
+            placements = list(copy.deepcopy(param.placements))
+            placements[-1] = Replicate()
             param = param.redistribute(
-                device_mesh=param.device_mesh, placements=[Replicate()]
+                device_mesh=param.device_mesh,
+                placements=placements,
             )
             state_dict[fqn_from_global_root] = param.to_local()
 

commit 0938a1e02287e84ef172dc067f83f7a8bbe0ba62

Add missing DeviceMesh import (#109187)
The test is broken after https://github.com/pytorch/pytorch/pull/107533#issuecomment-1709529759

Pull Request resolved: https://github.com/pytorch/pytorch/pull/109187
Approved by: https://github.com/clee2000

test/distributed/fsdp/test_hsdp_dtensor_state_dict.py
@@ -7,7 +7,13 @@ import torch
 import torch.nn as nn
 from torch.distributed._shard.sharded_tensor import ShardedTensor
 
-from torch.distributed._tensor import DTensor, mesh_resources, Replicate, Shard
+from torch.distributed._tensor import (
+    DeviceMesh,
+    DTensor,
+    mesh_resources,
+    Replicate,
+    Shard,
+)
 from torch.distributed._tensor.device_mesh import init_device_mesh
 from torch.distributed.fsdp import FullyShardedDataParallel as FSDP
 from torch.distributed.fsdp.api import (